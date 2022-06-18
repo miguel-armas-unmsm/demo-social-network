40cc234684c780bfdb32a3681989927f7bf1e9d1
Lorena Armas <miguel.armas@unmsm.edu.pe>
2022-06-18 09:06:44 -0500

    first commit

---
+++ api/components/user/network.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const response = require('../../../network/response');
+const controller = require('./controller');
+
+const router = express.Router();
+
+router.get('/', function(req, res) {
+    const list = controller.list(); 
+    response.success(req, res, list, 200);
+})
+
+module.exports = router;
